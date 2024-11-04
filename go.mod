018d6f88ff1203f5fa5b0f1de9385e12ec00aa22
bobJiang <374123140@qq.com>
2024-11-04 16:39:58 +0800

    add Fetch

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module go-crawler
+
+go 1.22.2
+
+require (
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
 	"strings"
 )
+
+// tag v0.0.4
+func Fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+
+	// 将 HTML 文本从特定编码转换为 UTF-8 编码
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		fmt.Printf("fetch error:%v\n", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
 
 // tag v0.0.3
 func main() {
